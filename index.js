5ffa4575c7cdc1e2f89cec413c17f85eedb769a7
pankaj8128 <pankajjagadale28@gmail.com>
2026-05-27 18:30:32 +0530

    `feat: add Docker deployment pipeline`

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ setupRoutes(app);
 
 handleSockets(wss);
 
-server.listen(3000, () => {
-  console.log("http://localhost:3000");
+server.listen(3000, "0.0.0.0", () => {
+  console.log("Server running");
 });
